e02679a847689e49ca37c2fdb33045d5ff04c886
Lushenggang <lushg-tcxg@qq.com>
2018-11-26 17:40:26 +0800

    import

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,10 +1,11 @@
-import outline from './outline'
+import vueOutline from './outline'
 import 'simple-vue-tree'
 import 'simple-vue-tree/dist/lib/simple-tree.css'
 
+
 export default {
   install: function (Vue, opts = {}) {
-    Vue.directive('outline', outline)
-  },
-  outline
+    Vue.directive('outline', vueOutline)
+  }
 }
+export const outline = vueOutline
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import 'simple-vue-tree'
 import 'simple-vue-tree/dist/lib/simple-tree.css'
 
-import outline from '@/directives/outline'
+import { outline } from '@/directives'
 
 Vue.directive('outline', outline)
 
